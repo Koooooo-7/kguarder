9b4fb9cf2f3b3e4f62afee9dbf83d73e998f30d2
koy <koy@ko8e24.top>
2023-08-09 20:02:44 +0800

    update: recover, simple recover resolution.

---
+++ kguarder-core/src/main/java/top/kguarder/core/annotation/Recover.java
@@ -0,0 +1,21 @@
+package top.kguarder.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Recover {
+
+    /*
+     * The bean name, which is the instance of {@link Fallbacker}
+     * Fallback the method result, support return any result of the method return type sub classes
+     * Ideally, The fallback logic should be simple and with error handling is better
+     *
+     * @return fallback bean name
+     */
+    String fallback() default "";
+
+}
