7d3a3ce6edb73689b5392f95cddbc78e87788189
koy <koy@ko8e24.top>
2023-08-24 22:15:53 +0800

    update: advice order.

--- kguarder-core/src/main/java/top/ko8e24/kguarder/core/configuration/GuarderProperties.java
+++ kguarder-core/src/main/java/top/ko8e24/kguarder/core/configuration/GuarderProperties.java
@@ -10,11 +10,11 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 public class GuarderProperties {
 
     /**
-     * Normally this should be after some annotations behavior sucjh as @Transaction, @Cacheable.
-     * By default the Order is {@value org.springframework.core.Ordered#LOWEST_PRECEDENCE +1}
+     * Normally this should be exec before some annotations behavior such as @Transaction, @Cacheable.
+     * By default the Order is {@value org.springframework.core.Ordered#LOWEST_PRECEDENCE -1}
      * adviceOrderAdjust, the adviceOrderAdjust - based on {@value org.springframework.core.Ordered#LOWEST_PRECEDENCE}
      */
-    private int adviceOrderAdjust = 0;
+    private int adviceOrderAdjust = -1;
 
     @NestedConfigurationProperty
     private ExecutorProperties executor = new ExecutorProperties();
